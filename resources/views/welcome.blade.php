6f10339909263aac669ddd08538b6d0cd6ebe3da
Kyle Bernard Alba Trimidal <kylealba0624@gmail.com>
2026-02-25 01:53:43 +0800

    making the website responsive

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/png" href="{{ asset('images/logo.png') }}">
-    <title>BLink - Home</title>
+    <title>Blink - Home</title>
     @vite(['resources/css/app.css'])
 </head>
 
@@ -55,6 +55,11 @@
                     </div>
                 </div>
                 <div class="product-card">
+                    <div class="product-info">
+                        <h3>Pineapple Jam</h3>
+                    </div>
+                </div>
+                 <div class="product-card">
                     <div class="product-info">
                         <h3>Pineapple Jam</h3>
                     </div>
